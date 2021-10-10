037d4673d9487318d954bcf9efaa0cb193f38847
Luis Perez <44172765+Lap343@users.noreply.github.com>
2021-10-10 13:26:44 -0700

    added earth and my face. positioned them in different parts of the space. Made the camera zoom out on scroll. Placed and styled markup so the main page is scrollable.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import './style.css';
 import * as THREE from 'three';
+// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const scene = new THREE.Scene();
 
@@ -22,7 +23,74 @@ const torus = new THREE.Mesh(geometry, material);
 scene.add(torus);
 
 const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position(5,5,5);
+pointLight.position.set(5,5,5);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
+
+// const lightHelper = new THREE.PointLightHelper(pointLight);
+// const gridHelper = new THREE.GridHelper(200, 50);
+// scene.add(lightHelper, gridHelper);
+
+// const controls = new OrbitControls(camera, renderer.domElement);
+
+const addStar = () => {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+  const star = new THREE.Mesh(geometry, material);
+
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+
+Array(200).fill().forEach(addStar);
+
+const spaceTexture = new THREE.TextureLoader().load('./assets/space.jpg');
+scene.background = spaceTexture;
+
+const luisTexture = new THREE.TextureLoader().load('./assets/me.jpg');
+
+const luis = new THREE.Mesh(
+  new THREE.BoxGeometry(3,3,3),
+  new THREE.MeshBasicMaterial({ map: luisTexture })
+  );
+  
+  scene.add(luis);
+  
+  const earthTexture = new THREE.TextureLoader().load('./assets/earth.jpg');
+  const normalTexture = new THREE.TextureLoader().load('./assets/earthnormal.jpg');
+
+const earth = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial({ map: earthTexture, normalMap: normalTexture })
+);
+
+scene.add(earth);
+
+earth.position.z = 30;
+earth.position.setX(-10);
+
+luis.position.z = -5;
+luis.position.x = 2;
+
+const moveCamera = () => {
+  const t = document.body.getBoundingClientRect().top;
+  earth.rotation.x += 0.05;
+  earth.rotation.y += 0.075;
+  earth.rotation.z += 0.05;
+
+  luis.rotation.y += 0.01;
+  luis.rotation.z += 0.01;
+
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.0002;
+  camera.position.y = t * -0.0002;
+}
+
+document.body.onscroll = moveCamera;
+moveCamera();
 
 const animate = () => {
   requestAnimationFrame(animate);
@@ -30,6 +98,8 @@ const animate = () => {
   torus.rotation.x += 0.01;
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
+
+  // controls.update();
 
   renderer.render(scene, camera);
 }
